ba966fc76c1d8c9689378e568bf900af83dd604b
Pierre Pinto de Oliveira <p.pinto2oliveira@gmail.com>
2021-11-05 13:04:04 +0100

    :sparkles: Added lot of tings lol

--- js/Point.js
+++ js/Point.js
@@ -1,16 +1,80 @@
 import * as THREE from './libraries/three/build/three.module.js';
+import { GUI } from './libraries/three/examples/jsm/libs/dat.gui.module.js';
 
 function Point (parent, pos, dir) {
     this.parent = parent
     this.pos = pos
     this.dir = dir
 
-    // this.setPosition = function (parent) {
-    //     this.parent = parent
-    //     let newPos = new THREE.Vector3()
-    //     newPos.addVectors(this.pos, this.parent.pos)
-    //     return new Particle(this.parent, newPos)
-    // }
+    this.getRandomArbitrary = function (min, max) {
+        return Math.random() * (max - min) + min
+      }
+
+    this.createNewBranch = function(nbNoeuds, scene, currentPoint) {
+
+        // let currentPoint = new Point(null, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 1, 0))
+
+        const points = [];
+        for (let i = nbNoeuds; i > 0; i -= 0.05) {
+            let newPos = new THREE.Vector3()
+            let newDir = new THREE.Vector3(this.getRandomArbitrary(0, i), this.getRandomArbitrary(0, i), this.getRandomArbitrary(0, i))
+            newPos.addVectors(currentPoint.pos, currentPoint.dir)
+
+            let point = new Point(currentPoint, newPos, newDir)
+            
+            points.push(point.pos)
+
+            currentPoint = point
+        }
+
+        
+        const textureLoader = new THREE.TextureLoader()
+        const particleTexture = textureLoader.load('./png/circle_05_t.png')
+
+        const material = new THREE.PointsMaterial({
+            size: .1,
+            sizeAttenuation: true,
+            color: 0xffbf00,
+            transparent: true,
+            alphaMap: particleTexture,
+            //alphaTest: 0.001h
+            depthTest: false
+        });
+
+        // GUI
+
+        let count = {
+            particles: 100
+        }
+        
+        let initGUI = function () {
+
+            const gui = new GUI();
+
+            gui.add( count, "particles", 0, nbNoeuds, 1 ).onChange( function ( value ) {
+
+                geometry.setDrawRange( 0,  nbNoeuds);
+
+            } );
+
+        }
+
+        const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+        geometry.setDrawRange(0, points.length)
+        const line = new THREE.Points( geometry, material );
+        // line.rotation.x = -Math.PI / 6
+        // initGUI()
+        scene.add( line );
+
+        const branch = {
+            line: line,
+            points: points,
+            material: material
+        }
+
+        return branch;
+    }
 }
 
 export default Point;
--- js/main.js
+++ js/main.js
@@ -4,77 +4,113 @@ import { GUI } from './libraries/three/examples/jsm/libs/dat.gui.module.js'
 import Point from './Point.js'
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
 
 const boxX = 4
 const boxY = 4
 const boxZ = 4
+let mouseX = 0
+let mouseY = 0
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-camera.position.set(5, 5, 5);
+camera.position.set(-10.58, 14.94, -58.57);
 
 // AMBIANT LIGHT
 const ambiantLight = new THREE.AmbientLight( 0x404040 ); // soft white light
 scene.add( ambiantLight );
 
 // LIGHT
-const light = new THREE.PointLight( 0xfff200, 20, 20, 20);
+const light = new THREE.PointLight( 0xffa94d, 30, 20);
 light.position.x = 0
-light.position.y = 0
+light.position.y = 2
 light.position.z = 0
 scene.add( light );
 
+// FOG
+scene.fog = new THREE.Fog(0x000000, 50, 100)
+
 // PLANE
-const planeGeometry = new THREE.PlaneGeometry( 10, 10, 10, 10 );
-const planeMaterial = new THREE.MeshPhongMaterial( {color: 0xffa94d} );
+const planeGeometry = new THREE.PlaneGeometry( 150, 150, 150, 150 );
+const planeMaterial = new THREE.MeshLambertMaterial( {color: 0x2a2a2a} );
 const plane = new THREE.Mesh( planeGeometry, planeMaterial );
 scene.add( plane ); 
-plane.position.set(0, -1, 0)
+plane.position.set(0, 0, 0)
 plane.rotation.x = -Math.PI/2
 
 // Utils functions
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min
 }
 
 // PARTICLE CURVE
+let currentPoint = new Point(null, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 1, 0))
 
-let nbPoints = 100
-let currentPoint = new Point(null, new THREE.Vector2(0, 0), new THREE.Vector2(0, 1))
-let nbIterations = 1000
-for (let i = 0; i <= nbIterations; i++)  {
+
+let nbNoeuds = 3
+const points = [];
+let branches = [];
+// points.push( new THREE.Vector3( 0, 0, 0 ) );
+// points.push( currentPoint.pos );
+for (let i = nbNoeuds; i > 0; i -= 0.05){
+  let newPos = new THREE.Vector3()
+
+  let newDir = new THREE.Vector3(getRandomArbitrary(0, -i), getRandomArbitrary(0, i), getRandomArbitrary(0, i))
+  //currentPoint.dir.multiplyScalar(5)
+  newPos.addVectors(currentPoint.pos, currentPoint.dir)
+
+  let point = new Point(currentPoint, newPos, newDir)
+
+  branches.push(point.createNewBranch(i, scene, point))
   
-  let point = new Point(currentPoint, newPos, )
+  points.push(point.pos)
+
+  currentPoint = point
+
 }
+const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('./png/circle_05_t.png')
 
-let particlesPosition = []
+const material = new THREE.PointsMaterial({
+	size: .1,
+  sizeAttenuation: true,
+  color: 0xffa94d,
+  transparent: true,
+  alphaMap: particleTexture,
+  depthTest: false
+});
 
-for (let i = 0; i < particles.length; i++) {
-  particlesPosition.push(particles[i].pos)
-}
 
-// CURVE
-console.log(particlesPosition)
-const curve = new THREE.SplineCurve( 
-  particlesPosition
- );
-
-const points = curve.getPoints( 50 );
 const geometry = new THREE.BufferGeometry().setFromPoints( points );
+//geometry.setDrawRange()
 
-const material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
-
-// Create the final object to add to the scene
-const splineObject = new THREE.Line( geometry, material );
-scene.add(splineObject)
+const line = new THREE.Points(geometry, material);
+scene.add( line );
 
 const moon = new THREE.AmbientLight( 0xffffff, 0.5 );
 scene.add( moon );
+
+// on MouseMove
+window.addEventListener('mousemove', (e) => {
+  mouseX = e.clientX
+  mouseY = e.clientY
+  let range = (mouseX * points.length) / window.innerWidth
+  let invMouseY = window.innerHeight - mouseY
+  let materialRange = (invMouseY * 2) / window.innerHeight
+  let branchRange = (mouseX * branches.length) / window.innerWidth
+  
+  material.size = materialRange
+  geometry.setDrawRange(0, range)
+  
+  for(let i = 0; i < branches.length; i++) {
+    branches[i].line.geometry.setDrawRange(0, branchRange)
+    branches[i].material.size = materialRange
+  }
+
+})
+
 
 // RESIZING
 window.addEventListener( 'resize', onWindowResize, false );
@@ -90,30 +126,22 @@ function onWindowResize(){
 
 // ORBIT CONTROLS
 const controls = new OrbitControls( camera, renderer.domElement );
+controls.enableDamping = true
+controls.enableDamping = true
+controls.enableZoom = true
+controls.maxPolarAngle = Math.PI / 2 - 0.1
+controls.rotateSpeed = 1
+controls.enableRotate = true
+controls.minDistance = 1
 controls.update();
-
-// GUI
-// const gui = new GUI()
-// const tubeFolder = gui.addFolder('Tree paramters')
-// tubeFolder.add(tubeParams, 'radius', 0, 4).onChange(() => {
-//   tube.geometry.parameters.radius = tubeParams.radius
-// })
-// tubeFolder.add(tubeParams, 'radialSegments', 0, 15).onChange(() => {
-//   tube.geometry.parameters.radialSegments = tubeParams.radialSegments
-// })
-// tubeFolder.add(tubeParams, 'tubularSegments', 0, 40).onChange(() => {
-//   tube.geometry.parameters.tubularSegments = tubeParams.tubularSegments
-// })
 
 const animate = function () {
   requestAnimationFrame( animate );
 
   // CONTROLS
   controls.update();
-  
 
-  // sphere.rotation.x += 0.01;
-  // sphere.rotation.y += 0.01;
+  //console.log(camera.position)
 
   renderer.render( scene, camera );
 };
